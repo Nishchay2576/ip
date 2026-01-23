de004df96a99feefa918700fe99c906366b0d7d7
Nishchay Sanghi <snishchay06@gmail.com>
2026-01-23 14:38:14 +0800

    Level-0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Minion User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Minion.java
@@ -0,0 +1,10 @@
+public class Minion {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Minion!");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
