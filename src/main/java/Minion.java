c90e688d0e44f64d5c7c8f9114f8775e1b90e6b4
Nishchay Sanghi <snishchay06@gmail.com>
2026-01-26 22:02:50 +0800

    Level-1

--- src/main/java/Minion.java
+++ src/main/java/Minion.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
 public class Minion {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Minion!");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello! I'm Minion!");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________");
+        String userInput = "";
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            userInput = in.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\t" + userInput);
+            System.out.println("\t____________________________________________________________");
+        }
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tBye. Hope to see you soon!");
+        System.out.println("\t____________________________________________________________");
     }
 }
