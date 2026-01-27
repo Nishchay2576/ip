8e6ef556f881b8db897c3ca64b7844c5538759d2
Nishchay Sanghi <snishchay06@gmail.com>
2026-01-27 14:58:24 +0800

    Level-2

--- src/main/java/Minion.java
+++ src/main/java/Minion.java
@@ -1,21 +1,41 @@
 import java.util.Scanner;
+
 public class Minion {
     public static void main(String[] args) {
+        String[] tasks = new String[100];
+        int taskCounter = 0;
+
         System.out.println("\t____________________________________________________________");
         System.out.println("\tHello! I'm Minion!");
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t____________________________________________________________");
-        String userInput = "";
+
         Scanner in = new Scanner(System.in);
+
         while (true) {
-            userInput = in.nextLine();
+            String userInput = in.nextLine();
+
             if (userInput.equalsIgnoreCase("bye")) {
                 break;
             }
+
+            if (userInput.equalsIgnoreCase("list")) {
+                System.out.println("\t____________________________________________________________");
+                for (int i = 1; i < taskCounter + 1; i++) {
+                    System.out.println("\t" + i + ". " + tasks[i - 1]);
+                }
+                System.out.println("\t____________________________________________________________");
+                continue;
+            }
+
             System.out.println("\t____________________________________________________________");
-            System.out.println("\t" + userInput);
+            System.out.println("\tadded: " + userInput);
             System.out.println("\t____________________________________________________________");
+
+            tasks[taskCounter] = userInput;
+            taskCounter++;
         }
+
         System.out.println("\t____________________________________________________________");
         System.out.println("\tBye. Hope to see you soon!");
         System.out.println("\t____________________________________________________________");
